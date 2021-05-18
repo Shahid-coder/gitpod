893962570316857e22256ce7dd7dabbfd0381757
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-05-18 14:15:13 +0000

    [ws-manager] Create services for all kinds of workspaces

--- components/ws-manager/pkg/manager/manager.go
+++ components/ws-manager/pkg/manager/manager.go
@@ -182,11 +182,8 @@ func (m *Manager) StartWorkspace(ctx context.Context, req *api.StartWorkspaceReq
 	}
 	tracing.LogEvent(span, "pod created")
 
-	// only regular workspaces get a service, the others are fine with just pod
+	// all workspaces get a service now
 	okResponse := &api.StartWorkspaceResponse{Url: startContext.WorkspaceURL}
-	if req.Type != api.WorkspaceType_REGULAR {
-		return okResponse, nil
-	}
 
 	// mandatory Theia service
 	servicePrefix := getServicePrefix(req)
